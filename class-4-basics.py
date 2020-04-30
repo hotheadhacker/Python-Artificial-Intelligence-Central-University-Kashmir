c52cbf9e2320f611cba0272e3939e8e146adfd4e
hotheadhacker <isalmanqureshi@gmail.com>
2020-04-30 15:22:57 +0530

    Class 4 notes and execution code

---
+++ class-4-basics.py
@@ -0,0 +1,36 @@
+# Class Dated 30/04/2020
+# Delivered By Prof. A. M Wani
+# Compiled By: Salman Qureshi
+
+print("Class Dated 30/04/2020")
+# Python – Data types—Strings (cont’d)
+# String  accessing values :
+char1 = "Hello World"
+str1 = "Python Programming"
+print(char1)
+print(str1)
+
+# String  accessing values :
+
+char1 = "Hello Python"
+str1 = " Hi"
+str2 = char1+str1
+print(str2)
+
+
+"""
+
+"""
+
+var = "Python"
+print(len(var))
+print(var[2])
+print(var[-1])
+print(var[1:3])
+
+# var[0]='z' #incorrect
+var='z'+var[1:]
+print(var)
+# op: zython
+
+
---
+++ class-4-tuples.py
@@ -0,0 +1,32 @@
+# Class Dated 30/04/2020
+# Delivered By Prof. A. M Wani
+# Compiled By: Salman Qureshi
+
+"""
+Tuples:
+-are immutable
+-can hold any type of objs inc: lists, dictonary etc...
+Series of comma-separated  values
+Also mutable and enclosed in parenthesis and holding mix data type .
+Also sliced to create new tuple but does not change original tuple
+Addition(+) operator used to create a new tuple which is of more than two tuples
+Use * to repeat a tuple.
+
+"""
+
+tupleExample = ('python', '123', 'language', 7.8)
+tupleExample2 = (485, 'program')
+print(tupleExample)
+print(tupleExample2)
+print(tupleExample[1])
+print(tupleExample[1:2])
+print(tupleExample[2: ])
+print(tupleExample[1*2])
+print(tupleExample + tupleExample2)
+# Note:    Same result without print ()  with tupleExample  +  tupleExample2
+# Code examples and output are given in Chap8p108p115 for the section on More on Tuples 
+
+
+
+
+
